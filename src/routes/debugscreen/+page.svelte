d9f1077ea9134fd3d1cff28ddf2c2f61151d5d94
Ragavendaran Puliyadi <ragav@op.in.eu.org>
2025-01-16 01:49:56 +0530

    Add some assets and a HTML Debug screen

---
+++ src/routes/debugscreen/+page.svelte
@@ -0,0 +1,9 @@
+<img alt="Grid pattern for debugging screen pixels" src="16by9grid.png" />
+
+<style lang="scss">
+  img {
+    width: 1600px;
+    height: 900px;
+    max-width: none;
+  }
+</style>
